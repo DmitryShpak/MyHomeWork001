deb4228ca4799114253bd8dbdd94700e3e22df55
Karaton <karaton@inbox.ru>
2022-11-02 12:37:06 +0300

    Создан код первой задачи

--- lesson001/Program.cs
+++ lesson001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+string Sa = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string Sb = Console.ReadLine(); 
+int a = int.Parse(Sa);
+int b = int.Parse(Sb);
+if (a>b)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else 
+{
+    Console.WriteLine("Второе число больше первого");
+}
