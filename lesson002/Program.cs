2d1f3d9e9688efb2c48349500f35db688537b067
Karaton <karaton@inbox.ru>
2022-11-02 15:12:01 +0300

    Составлен код второй задачи

--- lesson002/Program.cs
+++ lesson002/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max) 
+{
+    max = b;
+}
+ if (c > max)
+ {
+    max=c;
+ }
+ Console.WriteLine(max);
