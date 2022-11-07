0a2971a8497dae33e20524dbd09795641d1fe2c3
Karaton <karaton@inbox.ru>
2022-11-07 15:13:43 +0300

    Создан код восьмой задачи

---
+++ lesson008/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите пятизначное число: ");
+string n = Console.ReadLine();
+
+void Number(string n)
+{
+  if (n[0]==n[4] && n[1]==n[3])
+  {
+    Console.WriteLine("Число палиндром.");
+  }
+  else 
+  Console.WriteLine("Число не палиндром.");
+}
+if (n!.Length == 5)
+{
+  Number(n);
+}
+else Console.WriteLine($"Введите пятизначное число");
