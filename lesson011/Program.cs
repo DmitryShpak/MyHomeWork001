457b6f47b7170df76c0a7987d90871d383234996
Karaton <karaton@inbox.ru>
2022-11-08 14:12:08 +0300

    Создан код одиннадцатой задачи

--- lesson011/Program.cs
+++ lesson011/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("  11  Hello, World!");
+﻿
+  Console.Write("Введите число A: ");
+  int numberA = int.Parse(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = int.Parse(Console.ReadLine());
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  
+    }
+
+ Console.Write ($"Ответ: {result}");
+  
