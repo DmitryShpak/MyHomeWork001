0116f431e056a8486311fdddeffc37b7a8088a43
Karaton <karaton@inbox.ru>
2022-11-08 19:34:44 +0300

    Создан код десятой задачи

--- lesson010/Program.cs
+++ lesson010/Program.cs
@@ -1,25 +1,2 @@
-﻿Console.Write("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
-
-void Cube(int[] cube){
-  int count = 0;
-  int length = cube.Length;
-  while (count <  length){
-    cube[count] = Convert.ToInt32(Math.Pow(count, 3));
-    count++;
-  }
-}
-
-void PrintArry(int[] coll){
-  int count1 = coll.Length;
-  int index = 0;
-  while(index < count1){
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
-} 
-
-int[] arry = new int[cube+1];
-Cube(arry);
-PrintArry(arry);
- 
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" 10 Hello, World!");
