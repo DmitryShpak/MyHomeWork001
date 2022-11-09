1486f0479908ca503b325f37a852bc77c9f72fed
Karaton <karaton@inbox.ru>
2022-11-09 09:17:41 +0300

    Создан код 13-ой задачи

--- lesson013/Program.cs
+++ lesson013/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 13 Hello, World!");
+﻿int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 10);
+    Console.Write(" " + Output (i) + " ");
+ }
+Console.Write("]");
+
+int Output (int a)
+{
+    return numbers[a];
+}
